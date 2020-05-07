bd50cb4e17483723ee5986655683d1e5324d7564
Thomas Harris <twharris.57@gmail.com>
2020-05-06 22:16:19 -0400

    Started work on adding cookie based login. Added login controller, validation event on controller action that checks if the user exists and is active (and if not, logs them out), and added a login page and controller action.

--- JobTracker/Data/ApplicationDbContext.cs
+++ JobTracker/Data/ApplicationDbContext.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using JobTracker.Data.Entities.Login;
 using Microsoft.EntityFrameworkCore;
 
 namespace JobTracker.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="options"></param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
-        { }
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
     }
 }
