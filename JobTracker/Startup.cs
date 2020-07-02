199e8b3235c56f8865a47552a97c1c0c36eb9511
ShadowOTE <twharris.57@gmail.com>
2020-07-01 23:13:47 -0400

    Updated target framework version to .NET Core 3.1. Updated nuget pack… (#1)
    * Updated target framework version to .NET Core 3.1. Updated nuget packages accordingly. Updated startup.cs to work with updated packages.
    
    Note - The ASP .NET Core nuget package is running v2.1.7 instead of 2.2.0. Updating to 2.2.0 causes the application to crash on launch. Dont have time to diagnose now; maybe Wes can look at it and has a quick fix ready.
    
    * Updated to current version of AspNetCore (v2.2.0) and fixed the runtime error by setting JobTracker Properties -> Debug section -> web server settings -> Hosting Model to out of process. If this issue reoccurs when deploying to the web server, a web config file can be added per this stack overflow answer:
    
    https://stackoverflow.com/a/59390437

--- JobTracker/Startup.cs
+++ JobTracker/Startup.cs
@@ -2,11 +2,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace JobTracker
 {
@@ -29,15 +28,14 @@ namespace JobTracker
 
             // Identities for Login
             services.AddDefaultIdentity<IdentityUser>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
+                .AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            // MVC setup
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,17 +44,22 @@ namespace JobTracker
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseAuthentication();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Inventory}/{action=Index}/{id?}");
+                    pattern: "{controller=Inventory}/{action=Index}/{id?}");
             });
         }
     }
